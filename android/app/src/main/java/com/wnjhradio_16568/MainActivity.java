e9c53296c7fdb61763eb80f1040cc1b8f9e5b82b
Crowdbotics <team@crowdbotics.com>
2020-05-03 18:17:57 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/wnjhradio_16568/MainActivity.java
@@ -0,0 +1,15 @@
+package com.wnjhradio_16568;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "wnjhradio_16568";
+    }
+}
